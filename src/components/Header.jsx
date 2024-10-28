a9f654094036ffc7890180e83a9df812babb40d3
Fasakin Henry <fasakinhenry@gmail.com>
2024-10-28 07:41:34 +0100

    feat: add header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => (
+  <header className="fixed top-0 left-0 w-full p-4 bg-transparent z-50">
+    <h1 className="text-white text-2xl font-bold">Map of Pi</h1>
+  </header>
+);
+
+export default Header;
