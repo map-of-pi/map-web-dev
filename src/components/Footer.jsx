4231185c77b9a00eeae068f91fe7a77aba9e8709
Fasakin Henry <fasakinhenry@gmail.com>
2024-10-28 07:44:25 +0100

    feat: add footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer className="py-12 bg-black text-white text-center">
+    <p>&copy; 2024 Map of Pi. All rights reserved.</p>
+  </footer>
+);
+
+export default Footer;
