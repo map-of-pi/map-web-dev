79e8fa4769f6f798b1fc73d6fe97bb8e5d31a501
Fasakin Henry <fasakinhenry@gmail.com>
2024-11-04 13:14:13 +0100

    chore: add images to team component

--- src/components/Team/index.jsx
+++ src/components/Team/index.jsx
@@ -4,30 +4,39 @@ import '@splidejs/react-splide/css';
 
 const team = [
   {
+    key: '2',
+    image:
+      'https://images.unsplash.com/photo-1543132220-4bf3de6e10ae?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    name: 'Philip Jennings',
+    role: 'Chief Executive Officer',
+  },
+  {
     key: '1',
     image:
       'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    name: 'John Bakers',
-    role: 'Sales Manager',
+    name: 'Daniel Swooncx',
+    role: 'CTO',
   },
   {
-    key: '2',
-    image: '/images/henry.png',
-    name: 'Henry Arthur',
-    role: 'General Manager',
+    key: '3',
+    image:
+      'https://images.unsplash.com/photo-1522529599102-193c0d76b5b6?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    name: 'Fasakin Henry',
+    role: 'Developer',
   },
-  { key: '3', image: '/images/louise.png', name: 'Louise Marvin', role: 'CEO' },
   {
     key: '4',
-    image: '/images/bode.png',
-    name: 'Bode Akinwunmi',
+    image:
+      'https://images.unsplash.com/flagged/photo-1595514191830-3e96a518989b?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    name: 'Mr. Ant',
     role: 'Social Media Manager',
   },
   {
     key: '5',
-    image: '/images/nguyen.png',
-    name: 'Nguyen Shane',
-    role: 'Photographer',
+    image:
+      'https://images.unsplash.com/photo-1479936343636-73cdc5aae0c3?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    name: 'Abigail Iyanu',
+    role: 'Product designer',
   },
 ];
 
@@ -35,10 +44,10 @@ function Team() {
   return (
     <>
       <section className='bg-[#F7E9D1] py-[80px] flex flex-col items-start justify-start gap-[60px] md:gap-[100px] overflow-hidden'>
-        <div className='app-container'>
-          <h1 className='flex flex-col font-medium text-[40px] leading-[100%] sm:text-[48px] md:text-[64px] lg:text-[80px] tracking-[-2%] text-[#101828]'>
+        <div className='w-full max-w-[1360px] px-5 md:px-10 mx-auto'>
+          <h1 className='flex flex-col font-bold text-[40px] leading-[100%] sm:text-[48px] md:text-[64px] lg:text-[80px] tracking-[-2%] text-[#101828]'>
             <span>Brains Behind</span>
-            <span>Our Vision.</span>
+            <span>Map of PI.</span>
           </h1>
         </div>
         <div>
