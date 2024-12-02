9b9bafee18605ed3097f86da90c46e736a0213a7
Fasakin Henry <fasakinhenry@gmail.com>
2024-12-02 19:12:30 +0100

    chore: move team picture to the team folder

--- src/components/Team/index.jsx
+++ src/components/Team/index.jsx
@@ -13,8 +13,8 @@ const team = [
   {
     key: '1',
     image:
-      'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    name: 'Daniel Swooncx',
+      './team/1.jpg',
+    name: 'Daniel Lee',
     role: 'CTO',
   },
   {
