2fadc3a0d36fc33b1fd46899bc6263dca75006d0
Fasakin Henry <fasakinhenry@gmail.com>
2024-12-02 20:50:52 +0100

    chore: add Oluwabukola Adesina picture & designation

--- src/components/Team/index.jsx
+++ src/components/Team/index.jsx
@@ -30,8 +30,8 @@ const team = [
   {
     key: '5',
     image:
-      'https://images.unsplash.com/photo-1479936343636-73cdc5aae0c3?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    name: 'Abigail Iyanu',
+      './team/oluwabukola-adesina.png',
+    name: 'Oluwabukola Adesina',
     role: 'Product designer',
   },
   {
